cbd0d33f20044d9ce1f5d7a53a00646644fe6421
MiguelosAguialos <aguiarmiguel65@gmail.com>
2024-11-19 07:02:37 -0300

    feat: turbine class

--- main.py
+++ main.py
@@ -5,6 +5,19 @@
 #   3. Qual é a configuração atual de monitoramento?
 
 from logger import logger
+from turbine import Turbine
 import xlsxwriter
 
+# Lista de objetos contendo a classe Turbina
+turbines = [Turbine(1, True), Turbine(2, True), Turbine(3, False)]
+
+# Define o status de operação da máquina
+operation_status = True
+
 logger.info('HyDrive - Monitoring System | Starting..')
+logger.info(f'Operation Status - Machine: {operation_status}')
+for turbine in turbines:
+    if turbine.status:
+        logger.info(f'Turbine {turbine.index} operation status: {turbine.status}')
+    else:
+        logger.warning(f'Turbine {turbine.index} operation status: {turbine.status}')
---
+++ turbine.py
@@ -0,0 +1,4 @@
+class Turbine:
+    def __init__(self, index, status):
+        self.index = index
+        self.status = status
